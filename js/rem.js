7725768ea984894ee74971d3db1ef0f042ba1a2c
runqing.bo <runqing.bo@shuyun.com>
2018-05-10 18:39:17 +0800

    页面修改，背景图

--- js/rem.js
+++ js/rem.js
@@ -4,11 +4,11 @@
         recalc = function () {
             var clientWidth = docEl.clientWidth;
             if (!clientWidth) return;
-            // if (clientWidth >= 750) {
+            // if (clientWidth >= 640) {
             //     docEl.style.fontSize = '100px';
             // }
             // else {
-            //     docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+            //     docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
             // }
             docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
         };

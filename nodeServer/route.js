33370cf766601530883eb1d6ec6a9c217eea6e48
chan.qin <chan.qin@shuyun.com>
2018-05-16 22:16:36 +0800

    alls

--- nodeServer/route.js
+++ nodeServer/route.js
@@ -8,15 +8,15 @@ var MockRoute = function (request, response) {
     switch (urlPath) {
         // 用户核销接口
         case "/alicrad/saveCancelCode":
-            util.fRestApi(response,'{failed: true, message: "核销成功", data: null}');
+            util.fRestApi(response,'{"failed": true, "message": "核销成功", "data": null}');
             break;
         // 八马领取优惠券
         case "/alicrad/rewardGifts":
-                util.fRestApi(response,'{"failed": true, message: "该手机号已领取过奖励", data:{id: null, tick_name: "新人30元优惠券", tick_code: "0", ticket_amount: "30", check_code: null,tick_name:"新人30元优惠券",ticket_amount:"30"}}');
+                util.fRestApi(response,'{"failed": true, "message": "该手机号已领取过奖励", "data":{"id": null, "tick_name": "新人30元优惠券", "tick_code": "0", "ticket_amount": "30", "check_code": null,"tick_name":"新人30元优惠券","ticket_amount":"30"}}');
             break;
         // 根据手机号查询是否领取过券
         case "/alicrad/getGifts":
-            util.fRestApi(response,'{"failed": true, message: "查询成功", data:{id: null, tick_name: "新人50元优惠券", tick_code: "0", ticket_amount: "30", check_code: null,tick_name:"新人30元优惠券",ticket_amount:"30"}}');
+            util.fRestApi(response,'{"failed": true, "message": "查询成功", "data":{"id": null, "tick_name": "新人50元优惠券", "tick_code": "0", "ticket_amount": "30", "check_code": null,"tick_name":"新人30元优惠券","ticket_amount":"30"}}');
             break;
         default:
             console.log('{"msg":"该方法尚未实现，请补充","path":"' + urlPath + '"}');
